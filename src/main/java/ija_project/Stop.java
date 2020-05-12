c75c33dd8d228893ee6134355995b7c922be2306
Daniel <xpatek08@stud.fit.vutbr.cz>
2020-05-12 22:07:16 +0200

    merge

--- src/main/java/ija_project/Stop.java
+++ src/main/java/ija_project/Stop.java
@@ -6,10 +6,19 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.util.StdConverter;
 import com.sun.javafx.fxml.builder.TriangleMeshBuilder;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.stage.PopupWindow;
+import javafx.util.Duration;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Shape;
+import java.lang.reflect.Field;
 import javafx.scene.shape.TriangleMesh;
 
 import java.util.ArrayList;
@@ -18,6 +27,8 @@ import java.util.List;
 @JsonDeserialize(converter = Stop.StopConstruct.class)
 @JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class)
 public class Stop implements Drawable{
+    @JsonIgnore
+    private static final String SQUARE_BUBBLE = "M24 1h-24v16.981h4v5.019l7-5.019h13z";
     private Coordinate coordinates;
     @JsonIgnore
     private Street street;
@@ -32,6 +43,33 @@ public class Stop implements Drawable{
         setGui();
     }
 
+    private Tooltip makeBubble(Tooltip tooltip) {
+
+        return tooltip;
+    }
+
+
+    public Tooltip hackTooltipStartTiming(Tooltip tooltip) {
+        tooltip.setStyle("-fx-font-size: 16px; -fx-shape: \"" + SQUARE_BUBBLE + "\";");
+        tooltip.setAnchorLocation(PopupWindow.AnchorLocation.WINDOW_BOTTOM_LEFT);
+
+        try {
+            Field fieldBehavior = tooltip.getClass().getDeclaredField("BEHAVIOR");
+            fieldBehavior.setAccessible(true);
+            Object objBehavior = fieldBehavior.get(tooltip);
+
+            Field fieldTimer = objBehavior.getClass().getDeclaredField("activationTimer");
+            fieldTimer.setAccessible(true);
+            Timeline objTimer = (Timeline) fieldTimer.get(objBehavior);
+
+            objTimer.getKeyFrames().clear();
+            objTimer.getKeyFrames().add(new KeyFrame(new Duration(10)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tooltip;
+    }
+
     @Override
     public List<Shape> getGui() {
         return gui;
@@ -40,6 +78,27 @@ public class Stop implements Drawable{
     private void setGui() {
         this.gui = new ArrayList<>();
         this.gui.add(new Circle(coordinates.getX(), coordinates.getY(), 7, Color.YELLOW));
+        Circle circle =new Circle(coordinates.getX(), coordinates.getY(), 7, Color.YELLOW);
+
+        circle.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent t) {
+                double x= coordinates.getX();
+                String x1 =String.valueOf(x);
+                Tooltip zastavka = new Tooltip(x1);
+                Tooltip.install(circle, hackTooltipStartTiming(zastavka));
+                circle.setStroke(Color.WHITE);
+            }
+        });
+        circle.setOnMouseExited(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent t) {
+                circle.setStroke(Color.YELLOW);
+            }
+        });
+        this.gui.add(circle);
     }
 
     public Coordinate getCoordinates() {
--- src/main/java/ija_project/Vehicle.java
+++ src/main/java/ija_project/Vehicle.java
@@ -7,6 +7,15 @@ import javafx.application.Platform;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Shape;
+import javafx.scene.text.Text;
+import javafx.stage.PopupWindow;
+import javafx.util.Duration;
+import javafx.event.EventHandler;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.MouseEvent;
+import java.lang.reflect.Field;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -14,6 +23,8 @@ import java.util.List;
 
 @JsonDeserialize(converter = Vehicle.VehicleConstruct.class)
 public class Vehicle implements Drawable, TimerMapUpdate {
+    @JsonIgnore
+    private static final String SQUARE_BUBBLE = "M24 1h-24v16.981h4v5.019l7-5.019h13z";
     private Coordinate position;
     private double speed;
     @JsonIgnore
@@ -35,6 +46,27 @@ public class Vehicle implements Drawable, TimerMapUpdate {
         setNumber();
     }
 
+    public Tooltip hackTooltipStartTiming(Tooltip tooltip) {
+        tooltip.setStyle("-fx-font-size: 16px; -fx-shape: \"" + SQUARE_BUBBLE + "\";");
+        tooltip.setAnchorLocation(PopupWindow.AnchorLocation.WINDOW_BOTTOM_LEFT);
+
+        try {
+            Field fieldBehavior = tooltip.getClass().getDeclaredField("BEHAVIOR");
+            fieldBehavior.setAccessible(true);
+            Object objBehavior = fieldBehavior.get(tooltip);
+
+            Field fieldTimer = objBehavior.getClass().getDeclaredField("activationTimer");
+            fieldTimer.setAccessible(true);
+            Timeline objTimer = (Timeline) fieldTimer.get(objBehavior);
+
+            objTimer.getKeyFrames().clear();
+            objTimer.getKeyFrames().add(new KeyFrame(new Duration(1)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tooltip;
+    }
+
     @Override
     public List<Shape> getGui() {
         return gui;
@@ -42,7 +74,21 @@ public class Vehicle implements Drawable, TimerMapUpdate {
 
     private void setGui() {
         this.gui = new ArrayList<>();
-        this.gui.add(new Circle(position.getX(), position.getY(), 8, Color.RED));
+        this.gui.add(new Circle(position.getX(), position.getY(), 12, Color.WHITE));
+        this.gui.add(new Circle(position.getX(), position.getY(), 10, Color.RED));
+        Text text =new Text(position.getX()-6, position.getY()+5,this.path.getNumber());
+        this.gui.add(text);
+        Circle circle=new Circle(position.getX(), position.getY(), 12,Color.rgb(0,0,0,0));
+        this.gui.add(circle);
+        circle.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+
+                Tooltip cassava2 = new Tooltip(path.getNumber());
+                Tooltip.install(circle, hackTooltipStartTiming(cassava2));
+                circle.setStroke(Color.WHITE);
+            }
+        });
     }
 
     private void setNumber() {
